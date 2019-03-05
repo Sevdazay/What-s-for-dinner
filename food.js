2eada8199ded9a8fa61a99c8d388c3b090bd5d01
Sevdazay <47332055+Sevdazay@users.noreply.github.com>
2019-03-05 14:03:39 -0500

    create food.js
    created food on khan academy and pasted it onto here

---
+++ food.js
@@ -0,0 +1,90 @@
+// wooden table
+fill(0, 0, 0);
+background(186, 145, 20); 
+
+ // plate
+ellipse(200, 200, 350, 350);
+fill(255, 255, 255);
+ellipse(200, 200, 325, 325); 
+//potatos
+fill(219, 185, 100);
+ellipse(114,150,50,39);
+
+fill(230, 218, 83);
+ellipse(150,133,50,62);
+
+fill(219, 185, 100);
+ellipse(184,150,50,37);
+
+fill(219, 185, 100);
+ellipse(250,165,50,50);
+
+fill(230, 218, 83);
+ellipse(119,181,50,67);
+
+fill(230, 218, 83);
+ellipse(150,185,50,50);
+
+fill(230, 218, 83);
+ellipse(193,181,44,60);
+
+fill(219, 185, 100);
+ellipse(233,196,59,50);
+
+//carrots
+fill(227, 140, 9);
+triangle(53,255,189,314,174,351);
+
+triangle(103,256,189,314,174,357);
+
+triangle(103,275,204,337,174,357);
+
+//salad
+fill(70, 214, 34);
+beginShape();
+curveVertex(349,  375);
+curveVertex(279,  344);
+curveVertex(279,  250);
+curveVertex(195,  263);
+curveVertex(297, 295);
+curveVertex(224, 200);
+endShape();
+
+beginShape();
+curveVertex(349,  375);
+curveVertex(279,  344);
+curveVertex(279,  250);
+curveVertex(339,  263);
+curveVertex(297, 295);
+curveVertex(224, 200);
+endShape();
+
+beginShape();
+curveVertex(287,  408);
+curveVertex(279,  344);
+curveVertex(313,  245);
+curveVertex(339,  193);
+curveVertex(307, 183);
+curveVertex(224, 200);
+endShape();
+
+beginShape();
+curveVertex(287,  344);
+curveVertex(279,  344);
+curveVertex(313,  179);
+curveVertex(339,  265);
+curveVertex(290, 183);
+curveVertex(224, 281);
+endShape();
+
+fill(255, 0, 0);
+ellipse(300,304,22,23);
+
+fill(255, 0, 0);
+ellipse(273,242,23,21);
+
+fill(255, 0, 0);
+ellipse(337,242,23,21);
+
+fill(255, 0, 0);
+ellipse(235,266,23,21);
